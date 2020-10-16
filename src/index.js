7664a0e28a0b840948aec92feac5eb5f0ee261ba
Sander Kallio <sander.kallio@tuni.fi>
2020-10-17 00:02:51 +0300

    Added spin.js added loader functions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 //  Controler for initializing Charts
 import init from "./data";
 import LineChart from "./LineChart";
+import { startLoader, removeLoader } from "./Loader";
 
 const main = async () => {
   // Hacktober Fest Issue: create a cool loader for when data is getting pulled in
   // Get all data
+  startLoader();
   const data = await init();
   console.log("Initialized data sets", Object.keys(data));
-
+  removeLoader();
   LineChart(data);
 };
 
